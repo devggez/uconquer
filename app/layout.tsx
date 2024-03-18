a63548139bcd82309f4df8451a55fe04903e89fe
Salman Saad <devsalmansaad@gmail.com>
2024-03-18 16:18:25 +0600

    redix ui setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 import NavBar from "./NavBar";
 
@@ -18,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <NavBar></NavBar>
-        </nav>
-        {children}
-        </body>
+        <Theme>
+          <nav>
+            <NavBar></NavBar>
+          </nav>
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
