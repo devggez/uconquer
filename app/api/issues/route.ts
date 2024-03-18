bc5969b6ba01362b592b619a10d995645c203ec3
Salman Saad <devsalmansaad@gmail.com>
2024-03-18 19:57:38 +0600

    implement clint side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,17 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-    title : z.string().min(1).max(255),
-    description : z.string().min(1)
-});
+import { useState } from "react";
+import { createIssueSchema } from "../../validationSchmemas";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
+    const [error,setError] = useState('');
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 })
+        return NextResponse.json(validation.error.format(), { status: 400 })
 
     const newIssue = await prisma.issue.create({
         data: {title: body.title, description: body.description}
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,43 +1,62 @@
 'use client';
-import { Button, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, Callout, CalloutIcon, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchmemas';
+import { z } from "zod";
+import { Text } from '@radix-ui/themes';
 
-interface IssueForm {
-    title: string;
-    description: string;
-
-}
+type IssueForm = z.infer< typeof createIssueSchema>;
 
 const newIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const { register, control, handleSubmit,formState: {errors} } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
+    const [error, setError] = useState('');
+
 
     return (
-        <form
-            className='max-w-xl space-y-4'
-            onSubmit={handleSubmit(async (data) => {
-                await axios.post('/api/issues', data);
-                router.push('/issues');
-            })}>
-            <h1>New Issue page</h1>
-            <TextField.Root>
-                <TextField.Input placeholder='title' {...register('title')} />
-            </TextField.Root>
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
-            />
+        <div className='max-w-xl'>
+            {error && (
+            <Callout.Root color='red'>
+                <Callout.Text> {error} </Callout.Text>
+            </Callout.Root>
+            )}
+            <form
+                className=' space-y-4'
+                onSubmit={handleSubmit (async (data) => {
+                    try {
+                        await axios.post('/api/issues', data);
+                        router.push('/issues');
+                    } catch (error) {
+                        setError("there is some problem in code")
+                    }
+
+                })}>
+
+                <h1>New Issue page</h1>
+
+                <TextField.Root>
+                    <TextField.Input placeholder='title' {...register('title')} />
+                </TextField.Root>
+                {errors.title && <Text color="red" as='p' > {errors.title.message} </Text>}
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+                />
+                {errors.description && <Text color='red' as='p' >{errors.description.message}</Text>}
 
 
-
-            <Button>Submit new issue</Button>
-        </form>
+                <Button>Submit new issue</Button>
+            </form>
+        </div>
     )
 }
 
