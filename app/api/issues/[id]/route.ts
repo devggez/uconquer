633d6317781b2e648f3a3017eb4b2f14dbcb90c5
Salman Saad <devsalmansaad@gmail.com>
2024-03-29 23:43:45 +0600

    edit route bug fix

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,13 @@
+import authOptions from "@/app/auth/authOptions";
 import { patchIssueSchema } from "@/app/validationSchmemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
 
-    // const session = await getServerSession(authOptions);
-    // if (!session) return NextResponse.json({}, { status: 401 });
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({}, { status: 401 });
 
     const body = await request.json();
     const validation = patchIssueSchema.safeParse(body);
